05adbd05ae2e9e352a69dd400794c4c8ded135b5
Ken <2770219+ken0x0a@users.noreply.github.com>
2019-09-15 16:02:38 +0900

    style(lint): apply lint fix

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
 // This file is to be imported for client code using the network interface.
 import {
-  PersistedQueryNetworkInterface,
   addPersistedQueries,
-} from './src/network_interface/ApolloNetworkInterface';
-
-import {
-  OutputMap,
-  getQueryDocumentKey,
-} from './src/common';
-
-export {
   PersistedQueryNetworkInterface,
-  addPersistedQueries,
-  getQueryDocumentKey,
-  OutputMap,
-};
+} from './network_interface/ApolloNetworkInterface'
+
+import { getQueryDocumentKey, OutputMap } from './common'
+
+export { PersistedQueryNetworkInterface, addPersistedQueries, getQueryDocumentKey, OutputMap }
